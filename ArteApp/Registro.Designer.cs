b675689f32475b98a131be473a05c2ab1870c15c
SEBASTIAN ESTRADA BUSTAMANTE <sebastian.estradab@comunidad.iush.edu.co>
2024-10-10 16:01:45 -0500

    Registro

--- ArteApp/Registro.Designer.cs
+++ ArteApp/Registro.Designer.cs
@@ -119,7 +119,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(133, 530);
+            this.button1.Location = new System.Drawing.Point(113, 525);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(142, 50);
             this.button1.TabIndex = 7;
