17a70ecadec5a0a68b7b5d3138d610a9786f247b
JUAN ESTEBAN ESCOBAR PIEDRAHITA <juan.escobarp@comunidad.iush.edu.co>
2024-10-17 16:41:42 -0500

    ñ

--- ArteApp/Form1.cs
+++ ArteApp/Form1.cs
@@ -27,10 +27,10 @@ namespace ArteApp
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        
+       
 
-        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
